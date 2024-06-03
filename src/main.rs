e9ea06afaf9802fdb396f078b6725bf0caa9a523
mr-munshi <realmrmunshi@gmail.com>
2024-06-03 13:28:15 +0600

    logging using browser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+#[macro_use]
+extern crate nickel;
+
 use chrono::prelude::*;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
+
+use nickel::Nickel;
 
 fn formatted_time_entry() -> String {
     let local: DateTime<Local> = Local::now();
@@ -18,17 +23,33 @@ fn record_entry_in_log(filename: &str, bytes: &[u8]) -> std::io::Result<()> {
     Ok(())
 }
 
-fn log_time(filename: &'static str) -> std::io::Result<()> {
+fn log_time(filename: &'static str) -> std::io::Result<String> {
     let entry = formatted_time_entry();
-    let bytes = entry.as_bytes();
+    {
+        let bytes = entry.as_bytes();
 
-    record_entry_in_log(filename, &bytes)?;
-    Ok(())
+        record_entry_in_log(filename, &bytes)?;
+    }
+    Ok(entry)
+}
+
+fn do_log_time() -> String {
+    match log_time("log.txt") {
+        Ok(..) => format!("File created!"),
+        Err(e) => format!("Error: {}", e),
+    }
 }
 
 fn main() {
-    match log_time("log.txt") {
-        Ok(..) => println!("File created!"),
-        Err(..) => println!("Error: could not create file."),
-    }
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            do_log_time()
+        }
+    });
+
+    server
+        .listen("127.0.0.1:6767")
+        .expect("Failed to launch server");
 }
